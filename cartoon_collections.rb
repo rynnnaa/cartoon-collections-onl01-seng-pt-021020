a22c3da4f7e2d0e6bbc3a27574eb8487b5aafe56
Ryna Truong <rynatruong@outlook.com>
2020-02-23 04:37:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,9 +25,11 @@ end
 def find_the_cheese(arr_of_str)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
-  if arr_of_str.include? cheese_types[i]
-    true
-  else
-    nil
+    if arr_of_str.include? cheese_types[i]
+      true
+    else
+      nil
+    end
   end
 end
+
